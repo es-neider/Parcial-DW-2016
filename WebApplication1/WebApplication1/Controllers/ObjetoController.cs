278fe1f6f34b037451f10ee002c6aed458aaeab4
Sheppar19 <es-neider19@hotmail.com>
2016-03-30 20:54:13 -0500

    s

--- WebApplication1/WebApplication1/Controllers/ObjetoController.cs
+++ WebApplication1/WebApplication1/Controllers/ObjetoController.cs
@@ -14,11 +14,11 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             List<Objeto> listaObjeto = new List<Objeto>();
-            Objeto objeto = new Objeto(2000, "TV LED", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/cerealeseltrigo/Desktop/tv_lcd.jpg");
-            Objeto objeto2 = new Objeto(3000, "Tijeras", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/cerealeseltrigo/Desktop/tv_lcd.jpg");
-            Objeto objeto3 = new Objeto(4000, "ps4", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/cerealeseltrigo/Desktop/tv_lcd.jpg");
-            Objeto objeto4 = new Objeto(5000, "xbox one", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/cerealeseltrigo/Desktop/tv_lcd.jpg");
-            Objeto objeto5 = new Objeto(6000, "consolador", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/cerealeseltrigo/Desktop/tv_lcd.jpg");
+            Objeto objeto = new Objeto(2000, "TV LED", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/josek/Desktop/og.jpg");
+            Objeto objeto2 = new Objeto(3000, "Tijeras", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/josek/Desktop/tv_lcd.jpg");
+            Objeto objeto3 = new Objeto(4000, "ps4", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/josek/Desktop/tv_lcd.jpg");
+            Objeto objeto4 = new Objeto(5000, "xbox one", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/josek/Desktop/tv_lcd.jpg");
+            Objeto objeto5 = new Objeto(6000, "iphone", "esta en buen estado", "27/03/1016 12:30 pm", "C:/Users/josek/Desktop/tv_lcd.jpg");
             listaObjeto.Add(objeto);
             listaObjeto.Add(objeto2);
             listaObjeto.Add(objeto3);
